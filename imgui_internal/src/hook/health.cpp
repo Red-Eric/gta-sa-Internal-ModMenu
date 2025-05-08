9a01410df39c772fa3524e415bcd541423f90672
Red <redson.eric.2004@gmail.com>
2025-05-08 09:18:29 +0200

    Add menu Imgui
    ImGui Menu Added

--- imgui_internal/src/hook/health.cpp
+++ imgui_internal/src/hook/health.cpp
@@ -2,15 +2,22 @@
 
 uintptr_t healthHookAddrss = gameBase + 0xB331F;
 uintptr_t jmpback = healthHookAddrss + 0x6;
-float* playerHealthAddrs = (float*)getPointerAddress((gameBase + 0x76F3B8), { 0x540 });
+float* playerHealthAddrs = (float*)getPointerAddress((gameBase + 0x76F3B8), { 0x540 }) + 0x00;
+
+// [400000+76F3B8]+540
 
 __declspec(naked) void ourfunc() {
 	
-	* playerHealthAddrs = 200;
 	__asm {
-		mov dword ptr[esi + 0x540], edx
-		jmp[jmpback]
+		cmp dword ptr [esi+0x544], 0x43300000
+		je exitCodeCaveHealth
+
+		normalHealthComp:
+			mov dword ptr[esi + 0x540], edx
+		exitCodeCaveHealth:
+			jmp[jmpback]
 	}
+	
 }
 
 void healthHook::ON()
